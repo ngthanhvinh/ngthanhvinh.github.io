21c214a83758c9b0c8a1650a73e64f88b74a5584
Vinh Nguyen <ngthanhvinh2000@gmail.com>
2019-07-10 09:46:57 +0700

    Fix Contact page

--- js/contact.js
+++ js/contact.js
@@ -1,73 +1,68 @@
-var words = document.getElementsByClassName('contact-word');
-var logos = document.getElementsByClassName('contact-logo');
+var words = document.getElementsByClassName("contact-word");
+var logos = document.getElementsByClassName("contact-logo");
 var wordArray = [];
 var currentWord = 0;
 var newWord = 0;
 
 words[currentWord].style.opacity = 1;
 for (var i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
+	splitLetters(words[i]);
 }
 
 function changeWord() {
-  if (currentWord === newWord) return;
+	if (currentWord === newWord) return;
 
-  var cw = wordArray[currentWord];
-  var nw = wordArray[newWord];
+	var cw = wordArray[currentWord];
+	var nw = wordArray[newWord];
 
-  for (var i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
-  }
+	for (var i = 0; i < cw.length; i++) {
+		animateLetterOut(cw, i);
+	}
 
-  for (var i = 0; i < nw.length; i++) {
-    nw[i].className = 'contact-letter behind';
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
-  }
+	for (var i = 0; i < nw.length; i++) {
+		nw[i].className = "contact-letter behind";
+		nw[0].parentElement.style.opacity = 1;
+		animateLetterIn(nw, i);
+	}
 
-  currentWord = newWord;
+	currentWord = newWord;
 }
 
 function animateLetterOut(cw, i) {
-  cw[i].className = 'contact-letter out';
+	cw[i].className = "contact-letter out";
 }
 
 function animateLetterIn(nw, i) {
-  setTimeout(function() {
-    nw[i].className = 'contact-letter in';
-  }, 100);
+	setTimeout(function() {
+		nw[i].className = "contact-letter in";
+	}, 100);
 }
 
 function splitLetters(word) {
-  var content = word.innerHTML;
-  word.innerHTML = '';
-  var letters = [];
-  for (var i = 0; i < content.length; i++) {
-    var letter = document.createElement('span');
-    letter.className = 'contact-letter';
-    letter.innerHTML = content.charAt(i) === ' ' ? `&nbsp;` : content.charAt(i);
-    word.appendChild(letter);
-    letters.push(letter);
-  }
+	var content = word.innerHTML;
+	word.innerHTML = "";
+	var letters = [];
+	for (var i = 0; i < content.length; i++) {
+		var letter = document.createElement("span");
+		letter.className = "contact-letter";
+		letter.innerHTML = content.charAt(i) === " " ? `&nbsp;` : content.charAt(i);
+		word.appendChild(letter);
+		letters.push(letter);
+	}
 
-  wordArray.push(letters);
+	wordArray.push(letters);
 }
 
-logosContainer = document.getElementById('contact-logos');
+logosContainer = document.getElementById("contact-logos");
 
-logosContainer.addEventListener('mouseleave', function() {
-  newWord = 0;
+logosContainer.addEventListener("mouseleave", function() {
+	newWord = 0;
 });
 
 for (let i = 0; i < logos.length; ++i) {
-  logos[i].addEventListener('mouseenter', function() {
-    logos[i].children[0].src =
-      logos[i].children[0].src.slice(0, -4) + '-hover.svg';
-    newWord = i + 1;
-  });
-  logos[i].addEventListener('mouseleave', function() {
-    logos[i].children[0].src = logos[i].children[0].src.slice(0, -10) + '.svg';
-  });
+	logos[i].addEventListener("mouseenter", function() {
+		newWord = i + 1;
+	});
 }
 
 var changeDelay = 300;
