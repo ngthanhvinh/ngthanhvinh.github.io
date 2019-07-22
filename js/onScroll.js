19d30cbe8c8a069f62a59a999418ac954a81ca6d
Vinh Nguyen <ngthanhvinh2000@gmail.com>
2019-07-22 09:29:05 +0700

    Add up arrow button

--- js/onScroll.js
+++ js/onScroll.js
@@ -1,12 +1,19 @@
 home = document.getElementsByClassName("home")[0];
+upArrow = document.getElementsByClassName("up-arrow")[0];
 navbar = document.getElementById("myNavbar");
 
 const handleScroll = () => {
-	const offset = home.offsetTop + home.offsetHeight;
-	if (window.pageYOffset > offset - 200) {
+	const offsetY = home.offsetTop + home.offsetHeight;
+	if (window.pageYOffset > offsetY - 200) {
 		navbar.style.background = "rgba(28, 28, 29, 0.8)";
 	} else {
 		navbar.style.background = "rgba(0, 0, 0, 0)";
+	}
+
+	if (window.pageYOffset > Math.max(50, offsetY - 500)) {
+		upArrow.style.visibility = "visible";
+	} else {
+		upArrow.style.visibility = "hidden";
 	}
 };
 
