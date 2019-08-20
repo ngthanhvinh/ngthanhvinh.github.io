5ce0b6c1253dabddff4789f9a333a6ae8c05dd83
Vinh Nguyen <ngthanhvinh2000@gmail.com>
2019-08-20 22:24:39 +0700

    Fix Projects page

--- js/flickity.js
+++ js/flickity.js
@@ -1,16 +1,10 @@
 $(document).ready(function() {
 	var $carousel = $(".carousel").flickity({
 		// options
-		contain: true,
-		wrapAround: true,
-		selectedAttraction: 0.01,
-		friction: 0.15
-		// adaptiveHeight: true
+		// contain: true,
+		wrapAround: true
 	});
 
 	// bind event listeners
 	$carousel.flickity("resize");
-	// $carousel.on("change.flickity", listener);
 });
-
-function listener() {}
