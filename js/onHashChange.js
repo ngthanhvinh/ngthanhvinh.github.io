b764ee16c16580e622f38744914dd5e4bb617608
Vinh Nguyen <ngthanhvinh2000@gmail.com>
2019-08-24 09:13:11 +0700

    Fix fullpage settings and delete setInterval in onHashChange.js

--- js/onHashChange.js
+++ js/onHashChange.js
@@ -1,35 +1,33 @@
 // Fix the navbar when the URL is changed
 function activeNavbar() {
-  $('#navbar-items a').removeClass('active');
+	$("#navbar-items a").removeClass("active");
 
-  var links = $('#navbar-items a');
-  var active = Boolean(false);
+	var links = $("#navbar-items a");
+	var active = Boolean(false);
 
-  $.each(links, function(key, va) {
-    if (va.href === document.URL) {
-      $(this).addClass('active');
-      $(this).focus();
-      active = true;
-    }
-  });
+	$.each(links, function(key, va) {
+		if (va.href === document.URL) {
+			$(this).addClass("active");
+			$(this).focus();
+			active = true;
+		}
+	});
 
-  if (!active) {
-    $(links[0]).addClass('active');
-  }
+	if (!active) {
+		$(links[0]).addClass("active");
+	}
 }
 
 // When location hash changes, run hashChangeHandler()
 function hashChangeHandler() {
-  activeNavbar();
+	activeNavbar();
 }
 
 $(function() {
-  hashChangeHandler();
-  $(window)
-    .bind('hashchange', function() {
-      hashChangeHandler();
-    })
-    .trigger('hashchange');
+	hashChangeHandler();
+	$(window)
+		.bind("hashchange", function() {
+			hashChangeHandler();
+		})
+		.trigger("hashchange");
 });
-
-setInterval(hashChangeHandler, 80);
